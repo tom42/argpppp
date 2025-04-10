b287828a8aa31d1852b97836d9a98ef73a8d9888
tom <tom@z2>
2025-04-10 22:21:47 +0200

    Turn argpppp-config.cmake into argpppp-config.cmake.in
    Also call check_required_components at end of file.
    Quoting
    https://cmake.org/cmake/help/latest/module/CMakePackageConfigHelpers.html:
    
    "check_required_components(<PackageName>) should be called at the end
    of the FooConfig.cmake file. This macro checks whether all requested,
    non-optional components have been found [...]."
    
    And
    
    "This macro should be called even if the package doesn't provide
    any components to make sure users are not specifying components
    erroneously."
    
    And that's why we're calling it.

--- cmake/InstallArgpppp.cmake
+++ cmake/InstallArgpppp.cmake
@@ -1,9 +1,11 @@
 # SPDX-FileCopyrightText: 2025 Thomas Mathys
 # SPDX-License-Identifier: MIT
 
-# Install argpppp
-# Based on https://crascit.com/2024/04/04/cxx-modules-cmake-shared-libraries/#h-installing-shared-libraries-with-c-20-modules
 include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
+
+# Install argpppp module and exports
+# Based on https://crascit.com/2024/04/04/cxx-modules-cmake-shared-libraries/#h-installing-shared-libraries-with-c-20-modules
 install(
   TARGETS argpppp
   EXPORT argpppp-targets
@@ -13,12 +15,16 @@ install(
     DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
   INCLUDES
     DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-
 install(
   EXPORT argpppp-targets
   DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/argpppp
   CXX_MODULES_DIRECTORY .)
 
+# Generate and install argpppp-config.cmake
+configure_package_config_file(
+  "${PROJECT_SOURCE_DIR}/cmake/argpppp-config.cmake.in"
+  "${PROJECT_BINARY_DIR}/argpppp-config.cmake"
+  INSTALL_DESTINATION lib/cmake/argpppp)
 install(
-  FILES ${PROJECT_SOURCE_DIR}/cmake/argpppp-config.cmake
-  DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/argpppp)
+  FILES "${PROJECT_BINARY_DIR}/argpppp-config.cmake"
+  DESTINATION lib/cmake/argpppp)
