ce65234cd4375c96c4868d2149723f1c9fa1daef
tom <tom@z2>
2025-04-12 22:16:51 +0200

    Separate generation of version and config file

--- cmake/InstallArgpppp.cmake
+++ cmake/InstallArgpppp.cmake
@@ -20,16 +20,21 @@ install(
   DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/argpppp
   CXX_MODULES_DIRECTORY .)
 
-# Generate and install argppppConfig.cmake and argppppConfigVersion.cmake
-configure_package_config_file(
-  "${PROJECT_SOURCE_DIR}/cmake/argppppConfig.cmake.in"
-  "${PROJECT_BINARY_DIR}/argppppConfig.cmake"
-  INSTALL_DESTINATION lib/cmake/argpppp)
+# Generate and install argppppConfigVersion.cmake
 write_basic_package_version_file(
   "${PROJECT_BINARY_DIR}/argppppConfigVersion.cmake"
   COMPATIBILITY SameMajorVersion)
 install(
   FILES
-  "${PROJECT_BINARY_DIR}/argppppConfig.cmake"
   "${PROJECT_BINARY_DIR}/argppppConfigVersion.cmake"
+  DESTINATION lib/cmake/argpppp)
+
+# Generate and install argppppConfig.cmake
+configure_package_config_file(
+  "${PROJECT_SOURCE_DIR}/cmake/argppppConfig.cmake.in"
+  "${PROJECT_BINARY_DIR}/argppppConfig.cmake"
+  INSTALL_DESTINATION lib/cmake/argpppp)
+install(
+  FILES
+  "${PROJECT_BINARY_DIR}/argppppConfig.cmake"
   DESTINATION lib/cmake/argpppp)
