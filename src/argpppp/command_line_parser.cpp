bcc42b2128cb678d766432476d51c2240ed3e07f
tom42 <tom42@users.noreply.github.com>
2025-09-11 21:00:04 +0200

    Add missing command_line_parser::parse

--- src/argpppp/command_line_parser.cpp
+++ src/argpppp/command_line_parser.cpp
@@ -1,6 +1,10 @@
 // SPDX-FileCopyrightText: 2025 Thomas Mathys
 // SPDX-License-Identifier: MIT
 
+module;
+
+#include <argp.h>
+
 module argpppp;
 
 namespace argpppp
@@ -10,6 +14,19 @@ void parse_command_line(int argc, char* argv[], const options& options)
 {
     command_line_parser parser;
     return parser.parse(argc, argv, options);
+}
+
+void command_line_parser::parse(int /*argc*/, char* /*argv*/[], const options& /*options*/) const
+{
+    // TODO: set up stuff for argp_parse (see old implementation)
+    // TODO: call argp_parse
+
+    constexpr const argp_child* children = nullptr;
+    constexpr const auto help_filter = nullptr;
+    constexpr const char* argp_domain = nullptr;
+
+    // TODO: supply all the commented out stuff
+    const argp argp{ {}/*argp_options.data()*/, {}/*parse_option_static*/, {}/*c_str(m_args_doc)*/, {}/*c_str(m_doc)*/, children, help_filter, argp_domain};
 }
 
 }
