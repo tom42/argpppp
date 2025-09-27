aea8d5c6cebb442de521cab279f0ac3d347c07a1
tom42 <tom42@users.noreply.github.com>
2025-09-27 21:54:14 +0200

    Test successful parsing of integers

--- test/argpppp_unit_test/option_handlers_test.cpp
+++ test/argpppp_unit_test/option_handlers_test.cpp
@@ -3,6 +3,8 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <cstdint>
+#include <utility>
+#include <variant>
 
 import argpppp;
 
@@ -14,7 +16,15 @@ TEST_CASE("value<std::signed_integral>")
     int32_t target;
     argpppp::value<int32_t> value(target);
 
-    // TODO: test successful parsing with default settings
+    SECTION("successful parsing with default settings")
+    {
+        CHECK(std::get<bool>(value.handle_option("-2147483648")) == true);
+        CHECK(target == -2147483648);
+
+        CHECK(std::get<bool>(value.handle_option("2147483647")) == true);
+        CHECK(target == 2147483647);
+    }
+
     // TODO: test too small/too big => appropriate error message
     // TODO: test bad input => appropriate error message
     // TODO: test alternate base setting (e.g. 0 for autodetect and maybe some nonstandard base such as 6 or something)
