f0ffc1efbf351c279373a5fe89ab67c53acf7801
tom42 <tom42@users.noreply.github.com>
2025-10-18 19:21:18 +0200

    get_context: return reference to context, not pointer.
    Rationale: there should be always a context, so we return a reference,
    not a pointer. We return a pointer if there is a possibility that we
    return nullptr, which we do not here.

--- src/argpppp/command_line_parser.cpp
+++ src/argpppp/command_line_parser.cpp
@@ -44,9 +44,9 @@ public:
     std::exception_ptr exception;
 };
 
-parser_context* get_context(argp_state* state) noexcept
+parser_context& get_context(argp_state* state) noexcept
 {
-    return static_cast<parser_context*>(state->input);
+    return *static_cast<parser_context*>(state->input);
 }
 
 }
@@ -85,16 +85,16 @@ parse_result command_line_parser::parse(int argc, char* argv[], const options& o
 
 error_t command_line_parser::parse_option_static(int key, char* arg, argp_state* state)
 {
-    auto context = get_context(state);
+    parser_context& context = get_context(state);
     try
     {
-        return context->this_parser.parse_option(key, arg, state);
+        return context.this_parser.parse_option(key, arg, state);
     }
     catch (...)
     {
         // Do not let exception escape into argp, which is written in C.
         // Instead, pass exception to calling C++ code through argpppp_context instance.
-        context->exception = std::current_exception();
+        context.exception = std::current_exception();
         return EINVAL;
     }
 }
@@ -108,8 +108,8 @@ error_t command_line_parser::parse_option(int key, char* arg, argp_state* state)
         case ARGP_KEY_END:
             return handle_key_end(state);
         default:
-            auto context = get_context(state);
-            const option_with_handler* owh = context->opts.find_option(key);
+            parser_context& context = get_context(state);
+            const option_with_handler* owh = context.opts.find_option(key);
             if (owh != nullptr)
             {
                 const auto handler_result = owh->handler()->handle_option(owh->opt(), arg);
@@ -121,23 +121,23 @@ error_t command_line_parser::parse_option(int key, char* arg, argp_state* state)
 
 error_t command_line_parser::handle_key_arg(char* arg, argp_state* state) const
 {
-    auto context = get_context(state);
+    parser_context& context = get_context(state);
 
-    if (context->result.args.size() >= context->opts.max_args())
+    if (context.result.args.size() >= context.opts.max_args())
     {
         report_failure(state, EXIT_FAILURE, 0, "too many arguments");
         return EINVAL;
     }
 
-    context->result.args.push_back(arg);
+    context.result.args.push_back(arg);
     return 0;
 }
 
 error_t command_line_parser::handle_key_end(argp_state* state) const
 {
-    auto context = get_context(state);
+    parser_context& context = get_context(state);
 
-    if (context->result.args.size() < context->opts.min_args())
+    if (context.result.args.size() < context.opts.min_args())
     {
         report_failure(state, EXIT_FAILURE, 0, "too few arguments");
         return EINVAL;
