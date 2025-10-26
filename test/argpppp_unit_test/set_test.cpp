08aa01c9f4bf8531b8d07f0376b9aea5ea421bfb
tom42 <tom42@users.noreply.github.com>
2025-10-26 18:35:37 +0100

    Set up test for setter forwarding

---
+++ test/argpppp_unit_test/set_test.cpp
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2025 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import argpppp;
+
+namespace argpppp_unit_test
+{
+
+TEST_CASE("set<string>")
+{
+    // TODO: implement
+}
+
+TEST_CASE("set<bool>")
+{
+    // TODO: implement
+}
+
+TEST_CASE("set<signed_integral>")
+{
+    // TODO: implement
+}
+
+}
