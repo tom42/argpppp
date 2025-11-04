414fcfb72a09ce727ef459b358c86d9b2f0dbb75
tom42 <tom42@users.noreply.github.com>
2025-11-04 22:42:29 +0100

    Set up test for signed_integral_argument_parser [skip ci]

---
+++ test/argpppp_unit_test/signed_integral_argument_parser_test.cpp
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2025 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import argpppp;
+
+namespace argpppp_unit_test
+{
+
+// TODO: implement (see value_test<signed_integral>, most of it should go here, probably)
+TEST_CASE("signed_integral_argument_parser")
+{
+}
+
+}
