34e29323abb30cddadee17fcf569d6278057c324
tom42 <tom42@users.noreply.github.com>
2025-11-18 22:53:51 +0100

    Further separate functions for reusability

--- src/argpppp/short_name.cpp
+++ src/argpppp/short_name.cpp
@@ -14,11 +14,16 @@ namespace argpppp
 namespace
 {
 
-bool is_valid_short_name(char c)
+bool is_printable(char c)
 {
     // cppreference:
     // "To use [ctype] functions safely with plain chars (or signed chars), the argument should first be converted to unsigned char"
-    return (c == '\0') || std::isprint(static_cast<unsigned char>(c));
+    return std::isprint(static_cast<unsigned char>(c));
+}
+
+bool is_valid_short_name(char c)
+{
+    return (c == '\0') || is_printable(c);
 }
 
 }
