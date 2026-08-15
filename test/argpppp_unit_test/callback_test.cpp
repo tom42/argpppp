99078d0c7a4f7e534f0341698732a9a22c131335
tom42 <tom42@users.noreply.github.com>
2026-08-15 10:18:34 +0200

    Move existing test code into section

--- test/argpppp_unit_test/callback_test.cpp
+++ test/argpppp_unit_test/callback_test.cpp
@@ -12,12 +12,15 @@ using argpppp::error;
 
 TEST_CASE("callback")
 {
-    argpppp::option opt('s', {}, {}, "STRING");
-    argpppp::callback callback([](const argpppp::option& o, const char* a) { return error(o, a, "horrible error"); });
+    SECTION("with option parameter")
+    {
+        argpppp::option opt('s', {}, {}, "STRING");
+        argpppp::callback callback([](const argpppp::option& o, const char* a) { return error(o, a, "horrible error"); });
 
-    auto result = callback.handle_option(opt, "argh!");
+        auto result = callback.handle_option(opt, "argh!");
 
-    CHECK(result == error("invalid argument 'argh!' for option '-s': horrible error"));
+        CHECK(result == error("invalid argument 'argh!' for option '-s': horrible error"));
+    }
 }
 
 }
