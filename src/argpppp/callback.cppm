077c71b2457554055cde6ab10f16ccbb92b4b5e1
tom42 <tom42@users.noreply.github.com>
2026-08-18 23:51:51 +0200

    Callback: remove old variant of handle_option

--- src/argpppp/callback.cppm
+++ src/argpppp/callback.cppm
@@ -22,11 +22,6 @@ public:
     explicit callback(std::function<option_handler_result(option_occurrence)> callback)
         : m_callback(std::move(callback)) {}
 
-    option_handler_result handle_option(const option& opt, const char* arg) const override
-    {
-        return m_callback(option_occurrence(opt, arg));
-    }
-
     virtual option_handler_result handle_option(option_occurrence opt) const override
     {
         return m_callback(opt);
--- test/argpppp_unit_test/callback_test.cpp
+++ test/argpppp_unit_test/callback_test.cpp
@@ -12,6 +12,7 @@ using argpppp::callback;
 using argpppp::error;
 using argpppp::ok;
 using argpppp::option;
+using argpppp::option_occurrence;
 
 TEST_CASE("callback")
 {
@@ -20,7 +21,7 @@ TEST_CASE("callback")
         option opt({}, "--switch");
         callback callback([&] { return error("error 1"); });
 
-        auto result = callback.handle_option(opt, nullptr);
+        auto result = callback.handle_option(option_occurrence(opt, nullptr));
 
         CHECK(result == error("error 1"));
     }
@@ -30,7 +31,7 @@ TEST_CASE("callback")
         option opt('s', {}, {}, "STRING");
         callback callback([](auto&& o) { return error(o, "error 2"); });
 
-        auto result = callback.handle_option(opt, "argh!");
+        auto result = callback.handle_option(option_occurrence(opt, "argh!"));
 
         CHECK(result == error("invalid argument 'argh!' for option '-s': error 2"));
     }
