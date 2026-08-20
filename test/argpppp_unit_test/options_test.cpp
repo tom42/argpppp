66e649b07be8429e9ab5717300aa193e47322858
tom42 <tom42@users.noreply.github.com>
2026-08-20 23:10:36 +0200

    Fix test: throw logic_error, exception does not take a message.
    std::exception taking a message is a non-standard extension of MSVC.

--- test/argpppp_unit_test/options_test.cpp
+++ test/argpppp_unit_test/options_test.cpp
@@ -30,7 +30,7 @@ class null_option_handler final : public option_handler
 public:
     option_handler_result handle_option(option_occurrence) const override
     {
-        throw std::exception("null_option_handler does not implement handle_option");
+        throw std::logic_error("null_option_handler does not implement handle_option");
     }
 };
 
