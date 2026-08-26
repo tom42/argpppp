d3acd1962f38edc737d23ff11be39fd814929933
tom42 <tom42@users.noreply.github.com>
2026-08-26 02:58:21 +0200

    Sketch out test

--- test/argpppp_unit_test/callback_test.cpp
+++ test/argpppp_unit_test/callback_test.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 #include <catch2/catch_test_macros.hpp>
+#include <functional>
 
 import argpppp;
 
@@ -12,6 +13,7 @@ using argpppp::callback;
 using argpppp::error;
 using argpppp::ok;
 using argpppp::option;
+using argpppp::option_handler_result;
 using argpppp::option_occurrence;
 
 TEST_CASE("callback")
@@ -34,6 +36,12 @@ TEST_CASE("callback")
         auto result = callback.handle_option(option_occurrence(opt, "argh!"));
 
         CHECK(result == error("invalid argument 'argh!' for option '-s': error 2"));
+    }
+
+    SECTION("no callback function passed to constructor")
+    {
+        callback(std::function<option_handler_result(void)>{}); // TODO: this should throw an exception
+        callback(std::function<option_handler_result(option_occurrence)>{}); // TODO: this should throw the same exception
     }
 }
 
